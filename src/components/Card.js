e69350d92d790a867888bd0fff20bec19a746ac8
Sahil Mehta <samehta91@gmail.com>
2018-03-15 21:28:51 -0700

    Add additional styling to image component

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,6 +8,10 @@ const defaultStyles = {
   boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
 };
 
+const hover = {
+  boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2)"
+};
+
 const Card = ({ className = "", style, children }) => {
   const styles = { ...defaultStyles, ...style };
   return (
--- src/components/CardImage.js
+++ src/components/CardImage.js
@@ -8,11 +8,21 @@ const defaultStyles = {
   objectFit: "cover"
 };
 
+const defaultImage = {
+  height: "200px"
+};
+
+const circularImage = {
+  borderRadius: "50%",
+  border: "1px solid #b2b2b2"
+};
+
 const CardImage = ({ style, imageSrc }) => {
   const styles = { ...defaultStyles, ...style}
+  const imageStyles = { ...defaultImage, ...circularImage };
   return (
     <div style={styles}>
-      <img className="card__image" src={imageSrc} style={} />
+      <img className="card__image" src={imageSrc} style={imageStyles} />
     </div>
   );
 };
